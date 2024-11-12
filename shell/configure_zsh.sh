ea3994bf9c6326b6d6e0b6b71731481e3384e954
Jiseong Bak <jiseongg301@gmail.com>
2024-11-12 13:37:26 +0900

    add plugins for zsh

--- shell/configure_zsh.sh
+++ shell/configure_zsh.sh
@@ -9,6 +9,16 @@ sed -i "" \
   "s/^ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/" \
   $HOME/.zshrc
 
+
+# plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i "" \
+  "s/^plugins=\(.*\)/plugins=\(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n\)/" \
+  $HOME/.zshrc
+
+
 # zshrc
 cp -rf $SHELL_DIR/zfunc $HOME/.zfunc
 cat $SHELL_DIR/zshrc >> $HOME/.zshrc
