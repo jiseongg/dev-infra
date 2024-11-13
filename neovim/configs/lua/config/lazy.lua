69aa6c4534844d5354eaf35e2543ee0e6b027bd3
Jiseong Bak <jiseongg301@gmail.com>
2024-11-13 10:03:20 +0900

    add go

--- neovim/configs/lua/config/lazy.lua
+++ neovim/configs/lua/config/lazy.lua
@@ -16,11 +16,10 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
