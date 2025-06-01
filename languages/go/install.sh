09d782f574173304af1137c461aba5f7f15f1e17
Jiseong Bak <jiseongg301@gmail.com>
2025-06-02 08:34:35 +0900

    go version up

--- languages/go/install.sh
+++ languages/go/install.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-script_dir=$(cd $(dirname $0); pwd)
+script_dir=$(
+	cd $(dirname $0)
+	pwd
+)
 
 GOROOT=$HOME/.go
 
-
 mkdir -p $GOROOT
-curl -L https://go.dev/dl/go1.23.3.darwin-arm64.tar.gz | \
-  tar -C $GOROOT --strip-components=1 -xz 
+curl -L https://go.dev/dl/go1.24.3.darwin-arm64.tar.gz |
+	tar -C $GOROOT --strip-components=1 -xz
 
-if [[ $? -eq 0 ]]; then 
-  cat $script_dir/zshrc >> $HOME/.zshrc
+if [[ $? -eq 0 ]]; then
+	cat $script_dir/zshrc >>$HOME/.zshrc
 fi
